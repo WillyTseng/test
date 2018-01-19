be3d091d58e64da142811db8dee75855fae9e02f
Willy <wade5511216@gmail.com>
2018-01-19 17:43:21 +0800

    UpTest

--- app/src/main/java/com/example/willy/test/MainActivity.java
+++ app/src/main/java/com/example/willy/test/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.willy.test;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.Console;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
